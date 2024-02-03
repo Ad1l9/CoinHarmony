b2840ec555fc1b592bddb835e2c9324d8756dbbd
Adil <99889589+Ad1l9@users.noreply.github.com>
2024-02-03 18:58:33 +0400

    init

---
+++ CoinHarmony/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoinHarmony.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(bool theme = false)
+        {
+            return View(theme);
+        }
+    }
+}
---
+++ CoinHarmony/Models/CryptoCurrency.cs
@@ -0,0 +1,17 @@
+﻿namespace CoinHarmony.Models
+{
+    public class CryptoCurrency
+    {
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+        // Əlavə atributlar əlavə edə bilərsiniz
+
+        public CryptoCurrency(string name, string symbol, decimal price)
+        {
+            Name = name;
+            Symbol = symbol;
+            Price = price;
+        }
+    }
+}
